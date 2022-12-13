aea3e62ee48d4308f60dcda1f183cee7b85853a2
HemanthSnowflake <120384454+HemanthSnowflake@users.noreply.github.com>
2022-12-13 19:29:50 +0530

    Update streamlit_app.py
    added breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ streamlit.title('Healthy Eating for everyone')
 
 streamlit.header('Eat what feels right for you body')
 streamlit.text('Whats new here')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal :DD')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
