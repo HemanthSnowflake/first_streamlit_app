ed36fed62886c572167554e3e75d80c403e75872
HemanthSnowflake <120384454+HemanthSnowflake@users.noreply.github.com>
2022-12-13 19:37:45 +0530

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,10 @@ streamlit.title('Healthy Eating for everyone')
 streamlit.header('Eat what feels right for you body')
 streamlit.text('Whats new here')
 
+    
+
+streamlit.header('🥣🥣Breakfast Menu🥣🥣')
+streamlit.text(🥗🥗'Omega 3 & Blueberry Oatmeal :DD')
+streamlit.text(🥑🥑'Kale, Spinach & Rocket Smoothie' 🍞🍞)
+streamlit.text(🐔🐔'Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal :DD')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
