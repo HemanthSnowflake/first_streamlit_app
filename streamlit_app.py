f78f7416b33f82c9c7cf9954549fa6c550fdd81e
HemanthSnowflake <120384454+HemanthSnowflake@users.noreply.github.com>
2023-01-08 16:39:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ except URLError as e:
 
 
 
-streamlit.header("My fruit load list Contains")
+streamlit.header("View Our Fruit Load List -  Add  your  Favorites!")
 
 #snowflake related Function
 def get_fruit_load_list(): 
@@ -87,6 +87,7 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 
@@ -101,6 +102,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
 
 streamlit.stop()
