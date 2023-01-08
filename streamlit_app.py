6e1fa696655ddfc9e59adf9070ac97f97584d88c
HemanthSnowflake <120384454+HemanthSnowflake@users.noreply.github.com>
2023-01-08 14:49:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,10 +40,10 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
     if not fruit_choice:
         streamlit_error("Please select a fruit to get info")
      else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         streamlit.dataframe(fruityvice_normalized)
-except URLError as e
+except URLError as e:
     streamlit.error()
 
  #       streamlit.write('The user entered ', fruit_choice)
@@ -54,7 +54,7 @@ except URLError as e
 
 
 
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # Normalise the o/p 
