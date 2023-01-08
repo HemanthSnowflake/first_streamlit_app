58aa714aa9369dd79af7c14cf171e4b823301dc6
HemanthSnowflake <120384454+HemanthSnowflake@users.noreply.github.com>
2023-01-08 16:06:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,9 +91,10 @@ if streamlit.button('Get fruit load list'):
 #streamlit.header("My friut load list")
     streamlit.dataframe(my_data_rows)
 
+    streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit') 
 streamlit.write('The user entered ', add_my_fruit)
 
 
 my_cur.execute(" insert into fruit_load_list values ('from Streamlit')")
-streamlit.stop()
+
